f49a716605b41bccf26dac79fa60e0420ba63db2
Janus <ktr84759@gmail.com>
2022-07-11 13:15:57 +0800

    Study-State

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React  from "react"; 
+import React, {useState} from "react"; 
 
- 
+
 const App = ()  => {
+//State 
+
+
+let [name,setName] = useState("Mike Wang");
+const changeNameHandler = ()=>{
+  setName("Jaanus Zeng");
+}
+
 
   return(
     <div>
-        
+        <h1>{name}</h1>
+        <button onClick={changeNameHandler}> Change </button>
+        <p>{name}</p>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 
 ReactDOM.render(
 <React.StrictMode>   
-  <App/>
+  <App/> //component
 </React.StrictMode>,
 document.querySelector("#root")
 );
